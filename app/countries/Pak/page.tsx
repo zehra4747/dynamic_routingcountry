6a1aa35414080bdee8c16a3ce65a9fa13f25ec70
Zehra <163136295+zehra4747@users.noreply.github.com>
2024-10-25 22:52:04 +0500

    Update page.tsx

--- app/countries/Pak/page.tsx
+++ app/countries/Pak/page.tsx
@@ -1,17 +1,1 @@
-import Navbar from "@/app/Navbar/page"
-import Link from "next/link"
 
-export default function Countries(){
-    return(
-        <div>
-            <Navbar/>
-          
-
-
-            <ul>
-                <li>Country: Pakistan </li>
-                <li>Capital: Islamabad</li>
-            </ul>
-        </div>
-    )
-}
