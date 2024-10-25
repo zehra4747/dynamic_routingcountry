7cc1126494ba6147ebbf9590d607c0e8bcc44948
Zehra <163136295+zehra4747@users.noreply.github.com>
2024-10-25 22:43:44 +0500

    Add files via upload

---
+++ app/countries/Pak/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/app/Navbar/page"
+import Link from "next/link"
+
+export default function Countries(){
+    return(
+        <div>
+            <Navbar/>
+          
+
+
+            <ul>
+                <li>Country: Pakistan </li>
+                <li>Capital: Islamabad</li>
+            </ul>
+        </div>
+    )
+}
